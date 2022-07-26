01636fab77c281d252933238b6f8a29f0f17754a
BongSuCHOI <76158466+BongSuCHOI@users.noreply.github.com>
2022-07-26 18:47:25 +0900

    refactor: changed cursor design & logic

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,7 +4,43 @@ import CustomAnimatedCursor from "../../lib";
 export default function App() {
     return (
         <div className="App">
-            <CustomAnimatedCursor />
+            <CustomAnimatedCursor
+                // dotColor={}
+                // polyLineColor={}
+                // dotSize={8}
+                // dotSpeed={2}
+            />
+            <div>
+                <p>TEXT TEST</p>
+                <a href="#">LINK TEST</a>
+                <br />
+                <br />
+                <button type="button">BUTTON TEST</button>
+                <br />
+                <br />
+                <button type="submit">SUBMIT TEST</button>
+                <br />
+                <br />
+                <input type="text" placeholder="INPUT TEXT TEST" />
+                <br />
+                <br />
+                <input type="password" placeholder="INPUT PASSWORD TEST" />
+                <br />
+                <br />
+                <input type="number" placeholder="INPUT NUMBER TEST" />
+                <br />
+                <br />
+                <input type="checkbox" placeholder="INPUT CHECKBOX TEST" />
+                <br />
+                <br />
+                <input type="radio" placeholder="INPUT RADIO TEST" />
+                <br />
+                <br />
+                <select>
+                    <option>SELECTBOX TEST 1</option>
+                    <option>SELECTBOX TEST 2</option>
+                </select>
+            </div>
         </div>
     );
 }
